e3bfbc590272258c09d0c4c7ddf2ee61834dfe2b
linkor <ladrenaline@mail.ru>
2021-07-17 18:16:17 -0600

    add functions enter_parameters and find_results

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import os
 
@@ -7,17 +8,22 @@ import os
 PATH_LINUX = os.getcwd() + "/webdrivers/chromedriver"
 PATH_WIN = "/webdrivers/chromedriver.exe"
 OS = os.uname()
+# caps = DesiredCapabilities().CHROME
+# caps["pageLoadStrategy"] = "normal"
+
 DRIVER = None
 
 
 def open_yandex_url():
-    global DRIVER
     """Пользователь заходит на сайт Яндекс: www.yandex.ru"""
+    global DRIVER
     if OS.sysname == 'Linux':
         DRIVER = webdriver.Chrome(PATH_LINUX)
+        DRIVER.get("https://yandex.ru/")
     if OS.sysname == "Windows":
         DRIVER = webdriver.Chrome(PATH_WIN)
-    DRIVER.get("https://yandex.ru/")
+        DRIVER.get("https://yandex.ru/")
+    
 
 
 def enter_question():
@@ -33,32 +39,47 @@ def find_curent_url():
         href = elem.get_attribute('href')
         if href == "https://www.avtodispetcher.ru/distance/":
             return elem
-    return None
 
 
 def go_to_curent_url(elem):
     """Найдя нужный результат с этого сайта – пользователь кликает на данном результате и переходит на сайт www.avtodispetcher.ru/distance/ """
     if elem:
         elem.click()
+        return elem
 
-    time.sleep(5)
-      
-    
+   
 def enter_locations():
-    global DRIVER
-    title = DRIVER.title
     """5. Убедившись, что открылась верная ссылка, пользователь вводит следующие значения в поля:
         a. Поле «Откуда» - «Тула»
         b. Поле «Куда» - «Санкт-Петербург»
         c. Поле «Расход топлива» - «9»
         d. Поле «Цена топлива» - «46»"""
+    global DRIVER
+    # тут нужно запилить проверку на корректность ссылки. туда ли пришел
+    DRIVER.switch_to_window(window_name=DRIVER.window_handles[0])
+    DRIVER.close()
+    DRIVER.switch_to_window(window_name=DRIVER.window_handles[0])
+
+    start_point = DRIVER.find_element_by_name("from")
+    start_point.send_keys("Тула")
+
+    end_point = DRIVER.find_element_by_name("to")
+    end_point.send_keys("Санкт-Петербург")
+
+    fuel = DRIVER.find_element_by_name("fc")
+    fuel.send_keys(Keys.CONTROL + "a")
+    fuel.send_keys("9")
+
+    fuel = DRIVER.find_element_by_name("fp")
+    fuel.send_keys(Keys.CONTROL + "a")
+    fuel.send_keys("46")
+
+
+def click_find_result():
+    """Пользователь нажимает кнопку «Рассчитать» """
+    submit = DRIVER.find_element_by_class_name("submit_button")
+    submit.click()
     time.sleep(10)
-    pass
-
-
-# def click_find_result():
-#     """Пользователь нажимает кнопку «Рассчитать» """
-#     pass
 
 
 # def check_results():
@@ -97,13 +118,15 @@ def main():
     elem = find_curent_url()
     if elem:
         go_to_curent_url(elem)
+        enter_locations()
+        click_find_result()
+    if not elem:
+        DRIVER.quit()
 
     # global DRIVER
     # DRIVER = webdriver.Chrome(PATH_LINUX)
-    # site = DRIVER.get("https://www.avtodispetcher.ru/distance/")
+    # DRIVER.get("https://www.avtodispetcher.ru/distance/")
     
-    enter_locations()
-    # click_find_result()
     # check_results()
     # click_change_trip()
     # enter_new_trip()
